360a074b9879fdb2ba5a5907ec51b37d2d3e4493
summerliu <summer.ycliu@gmail.com>
2019-11-28 11:36:25 +0800

    [Counter] Add reset, increase, and decrease button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+    const initialState = 0;
+    const [count, setCount] = useState(initialState);
+
+    useEffect(() => {
+        // Update the document title using the browser API
+        document.title = `You clicked ${count} times`;
+    });
 
     return (
         <div>
             <div>You clicked {count} times</div>
-            <button onClick={() => setCount(count + 1)}>Click</button>
+            <button onClick={() => setCount(initialState)}>Reset</button>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
         </div>
     );
 };
